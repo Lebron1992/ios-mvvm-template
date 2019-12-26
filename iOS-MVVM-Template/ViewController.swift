cf326df9015f263ae0728bbb61d775e8d0597828
Lebron <wenzhi.zeng@outook.com>
2019-12-26 17:57:02 +0800

    Initial Commit

---
+++ iOS-MVVM-Template/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-MVVM-Template
+//
+//  Created by 曾文志 on 2019/12/26.
+//  Copyright © 2019 Lebron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
